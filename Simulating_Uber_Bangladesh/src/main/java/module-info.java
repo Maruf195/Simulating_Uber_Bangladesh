149924c98609b04a4bfdc54fbfff0d5ec9fc5d22
Nujat-E- Hasnat <nujatehasnat11@gmail.com>
2025-05-14 19:12:18 +0600

    Delete Simulating_Uber_Bangladesh directory

--- Simulating_Uber_Bangladesh/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.simulating_uber_bangladesh {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.simulating_uber_bangladesh to javafx.fxml;
-    exports org.example.simulating_uber_bangladesh;
-}
